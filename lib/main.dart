c5295ccf15dbda123a2a845cd08968b51f0c04dd
Abdelrahman Sadek <abdelrahmansadek109@gmail.com>
2026-02-24 09:19:38 +0200

    senior

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     return MaterialApp(
-      title: "Grocery App",
+      title: "senior App",
       debugShowCheckedModeBanner: false,
       home: HomePage(), //ProductDetailsPage(), //CategoriesPage(),
     );
